fdd354df1b0de686bde07545097ddc83cbfb5307
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-06-09 16:07:10 +0200

    basic data structure php

---
+++ data.php
@@ -0,0 +1,37 @@
+<?php
+$arrMusic = [
+    {
+        "title" = "New Jersey";
+        "singer" = "Bon Jovi";
+        "year" = "1988";
+    }
+    {
+        "title" = "Live at Wembley 86";
+        "singer" = "Queen";
+        "year" = "1992";
+    }
+    {
+        "title" = "Ten's Summoner's Tales";
+        "singer" = "Sting";
+        "year" = "1993";
+    }
+    {
+        "title" = "Steve Gaddd Band";
+        "singer" = "Steve Gaddd Band";
+        "year" = "2018";
+    }
+    {
+        "title" = "Brave New World";
+        "singer" = "iron Maiden";
+        "year" = "2000";
+    }
+    {
+        "title" = "One more car; One more rider";
+        "singer" = "Eric Clapton";
+        "year" = "2002";
+    }
+];
+
+header('Content-Type: application/json');
+
+echo json_encode($arrMusic);
---
+++ script.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      arrMusic: [],
+    };
+  },
+
+  methods: {
+    getData() {
+      axios
+        .get(
+          "http://localhost:8080/esercizi%20pome%20php-backend/php-dischi-json/data.json"
+        )
+        .then((response) => (this.arrMusic = response.data));
+    },
+
+    created() {
+      this.getData();
+    },
+  },
+}).mount("#app");
